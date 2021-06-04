38b1bfeeeb8dc5a75a1ebf67bb94fb43668aa34e
박민한 <alsgks1218@naver.com>
2021-06-04 11:31:08 +0900

    add navi options

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default function Welcome() {
+export default function Welcome({ navigation }) {
   return (
     <View>
       <Text>Welcome</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <Text>Go to Create Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <Text>Go to Log in</Text>
+      </TouchableOpacity>
     </View>
   );
 }
